d9628c102e2887be4db7dfc47b141bb0f63b44d3
Andre Chia <41582334+AndreChia@users.noreply.github.com>
2020-03-30 20:27:16 +0800

    Add files via upload

--- postgresql.py
+++ postgresql.py
@@ -3,11 +3,9 @@
 import psycopg2
 from config import config
 
-def connect():
-    # Connect to the PostGreSQL Database Server
+def createTable(query):
     connection = None
     try:
-        # read connection paramteres
         params = config()
 
         # connect to the PostGreSQL Server
@@ -17,29 +15,58 @@ def connect():
         # Create a cursor
         cursor = connection.cursor()
 
-        # Creating a table
-        create_table_query = '''CREATE TABLE mobile
-              (ID INT PRIMARY KEY     NOT NULL,
-              MODEL           TEXT    NOT NULL,
-              PRICE         REAL); '''
+        # Inserting a row into the mobile table
+        cursor.execute(query)
+        connection.commit()
+        print('Table has been created')
+        cursor.close()
 
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
 
-        # Inserting a row
+def insertRow(id, name, price):
+    connection = None
+
+    try:
+        params = config()
+
+        # connect to the PostGreSQL Server
+        print('Connecting to the PostGreSQL Database Server...')
+        connection = psycopg2.connect(**params)
+
+        # Create a cursor
+        cursor = connection.cursor()
+
+        # Inserting a row into the mobile table
         cursor.execute('INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)',
-                       (42, 'ASUS ROG', 1300)
+                       (id, name, price)
                        )
+        connection.commit()
+        print('Row with id', id, 'has been inserted')
+        cursor.close()
 
-        # Reading from the table
-        postgres_read_query = "SELECT * FROM mobile"
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
+def readTable(tableName):
+    connection = None
+
+    try:
+        params = config()
+
+        # connect to the PostGreSQL Server
+        print('Connecting to the PostGreSQL Database Server...')
+        connection = psycopg2.connect(**params)
+
+        # Create a cursor
+        cursor = connection.cursor()
+
+        postgres_read_query = "SELECT * FROM " + tableName
         cursor.execute(postgres_read_query)
-
-        # Update the table
-        postgres_update_query = "UPDATE mobile SET price = %s WHERE id = %s"
-        cursor.execute(postgres_update_query, (1200, 39))
-
-        # Delete a row from the table
-        postgres_delete_query = "DELETE from mobile WHERE id = %s"
-        cursor.execute(postgres_delete_query, (39, ))
 
         # Reading from the table
         print("Selecting rows from mobile table using cursor.fetchall")
@@ -52,18 +79,70 @@ def connect():
             print("Model =", row[1])
             print("Price =", row[2], "\n")
 
-        # Commit the connection and change into the database
-        connection.commit()
 
-        # Close the connection
         cursor.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+
     finally:
         if connection is not None:
             connection.close()
             print('Database connection closed.')
 
+def updateRow(id, price):
+    connection = None
+
+    try:
+        params = config()
+        # connect to the PostGreSQL Server
+        print('Connecting to the PostGreSQL Database Server...')
+        connection = psycopg2.connect(**params)
+
+        # Create a cursor
+        cursor = connection.cursor()
+
+        # Update the table
+        postgres_update_query = "UPDATE mobile SET price = %s WHERE id = %s"
+        cursor.execute(postgres_update_query, (price, id))
+        connection.commit()
+        print('Row with id', id, 'has been updated')
+        cursor.close()
+
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
+def deleteRow(id):
+    connection = None
+
+    try:
+        params = config()
+        # connect to the PostGreSQL Server
+        print('Connecting to the PostGreSQL Database Server...')
+        connection = psycopg2.connect(**params)
+
+        # Create a cursor
+        cursor = connection.cursor()
+
+        # Update the table
+        postgres_delete_query = "DELETE from mobile WHERE id = %s"
+        cursor.execute(postgres_delete_query, (id, ))
+        connection.commit()
+        print('Row with id', id, 'has been deleted')
+        cursor.close()
+
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
+
 if __name__ == '__main__':
-    connect()
+    # createTable('''CREATE TABLE mobile
+    #               (ID INT PRIMARY KEY     NOT NULL,
+    #               MODEL           TEXT    NOT NULL,
+    #               PRICE         REAL); ''')
+    # insertRow(10, 'Google Pixel', 1000)
+    # updateRow(10, 1100)
+    # deleteRow(10)
+    readTable("mobile")
 
